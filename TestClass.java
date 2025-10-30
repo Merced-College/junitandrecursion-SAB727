cc0e1733fbea85aabcae9408b925b4ab02dd65f7
SAB727 <141465618+SAB727@users.noreply.github.com>
2025-10-30 17:26:29 +0000

    2

--- TestClass.java
+++ TestClass.java
@@ -5,7 +5,15 @@
 public class TestClass {
     public static void main (String[] args) {
         
-        System.out.println(countHi("xxhixxxhixxxx"));
+        System.out.println("Count of hi's: " + countHi("xxhixxxhixxxx"));
+        System.out.println("Count of hi's (no x before): " + countHi2("xhixhix"));
+        System.out.println("Count of 8's (if next to each other then double): " + count8(8818));
+        System.out.println("Count of cats in str: " + strCount("catcowcat", "cat"));
+        System.out.println("Count of cows in str: " + strCount("catcowcat", "cow"));
+        System.out.println("Count of dogs in str: " + strCount("catcowcat", "dog"));
+        System.out.println("String cleaned: " + stringClean("yyzzza"));
+        System.out.println("String cleaned: " + stringClean("abbbcdd"));
+        System.out.println("String cleaned: " + stringClean("Hello"));
 
     } // end main
 
@@ -26,5 +34,74 @@ public class TestClass {
             return 1 + countHi(str.substring(2));
 
         return countHi(str.substring(1));
+    } // end method
+
+    public static int countHi2(String str) {
+        // check for null string
+        if (str == null)
+            return 0;
+        
+        // base case
+        if (str.length() <= 1)
+            return 0;
+        // if x is before hi, skip hi
+        if (str.startsWith("xhi"))
+            return countHi2(str.substring(3));
+        // if hi found, count it and move past it
+        if (str.startsWith("hi"))
+            return 1 + countHi2(str.substring(2));
+
+        return countHi2(str.substring(1));
+    } // end method
+
+    public static int count8(int n) {
+        // base case
+        if (n == 0)
+            return 0;
+
+        int lastDigit = n % 10;
+        int secondLastDigit = (n / 10) % 10;
+
+        if (lastDigit == 8) {
+            if (secondLastDigit == 8)
+                return 2 + count8(n / 10);
+            else
+                return 1 + count8(n / 10);
+        } else {
+            return count8(n / 10);
+        }
+    } // end method
+
+    public static int strCount(String str, String sub) {
+        // check for null string
+        if (str == null || sub == null)
+            return 0;
+        
+        // base case
+        if (str.length() < sub.length())
+            return 0;
+
+        if (str.startsWith(sub))
+            return 1 + strCount(str.substring(sub.length()), sub);
+
+        return strCount(str.substring(1), sub);
+    } // end method
+
+    public static String stringClean(String str) {
+        // check for null string
+        if (str == null)
+            return null;
+        
+        // base case
+        if (str.length() <= 1)
+            return str;
+
+        char firstLetter = str.charAt(0);
+        char secondLetter = str.charAt(1);
+
+        if (firstLetter == secondLetter)
+            return stringClean(str.substring(1));
+
+        return firstLetter + stringClean(str.substring(1));
     } // end method
 } // end class
--- UnitTest.java
+++ UnitTest.java
@@ -4,71 +4,256 @@ public class UnitTest {
 
     // first JUnit test
     @org.junit.Test
-    public void testCountHi1() {
+    public void test1CountHi() {
         int result = TestClass.countHi("xxhixxxhixxxx");
         org.junit.Assert.assertEquals(2, result);
     }
     
     // second JUnit test
     @org.junit.Test
-    public void testCountHi2() {
+    public void test2CountHi() {
         int result = TestClass.countHi("hihih");
         org.junit.Assert.assertEquals(2, result);
     }
 
     // third JUnit test
     @org.junit.Test
-    public void testCountHi3() {
+    public void test3CountHi() {
         int result = TestClass.countHi("hello");
         org.junit.Assert.assertEquals(0, result);
     }
 
     // fourth JUnit test
     @org.junit.Test
-    public void testCountHi4() {
+    public void test4CountHi() {
         int result = TestClass.countHi("abc");
         org.junit.Assert.assertEquals(0, result);
     }
 
     // fifth JUnit test
     @org.junit.Test
-    public void testCountHi5() {
+    public void test5CountHi() {
         int result = TestClass.countHi(null);
         org.junit.Assert.assertEquals(0, result);
     }
 
     // sixth JUnit test
     @org.junit.Test
-    public void testCountHi6() {
+    public void test6CountHi() {
         int result = TestClass.countHi("");
         org.junit.Assert.assertEquals(0, result);
     }
 
     // seventh JUnit test
     @org.junit.Test
-    public void testCountHi7() {
+    public void test7CountHi() {
         int result = TestClass.countHi("hihihi");
         org.junit.Assert.assertEquals(3, result);
     }
 
     // eighth JUnit test
     @org.junit.Test
-    public void testCountHi8() {
+    public void test8CountHi() {
         int result = TestClass.countHi("xhixhix");
         org.junit.Assert.assertEquals(2, result);
     }
 
     // ninth JUnit test
     @org.junit.Test
-    public void testCountHi9() {
+    public void test9CountHi() {
         int result = TestClass.countHi("hihohi");
         org.junit.Assert.assertEquals(2, result);
     }
 
     // tenth JUnit test
     @org.junit.Test
-    public void testCountHi10() {
+    public void test10CountHi() {
         int result = TestClass.countHi("hihihello");
         org.junit.Assert.assertEquals(2, result);
     }
+
+    // countHi2 tests
+    @org.junit.Test
+    public void test1CountHi2() {
+        int result = TestClass.countHi2("xxhixxxhixxxx");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test2CountHi2() {
+        int result = TestClass.countHi2("xhixhix");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test3CountHi2() {
+        int result = TestClass.countHi2("hihihi");
+        org.junit.Assert.assertEquals(3, result);
+    }
+
+    @org.junit.Test
+    public void test4CountHi2() {
+        int result = TestClass.countHi2("hixhihixhi");
+        org.junit.Assert.assertEquals(2, result);
+    }
+
+    @org.junit.Test
+    public void test5CountHi2() {
+        int result = TestClass.countHi2("xhixhihixhi");
+        org.junit.Assert.assertEquals(1, result);   
+    }
+
+    @org.junit.Test
+    public void test6CountHi2() {
+        int result = TestClass.countHi2("hello");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test7CountHi2() {
+        int result = TestClass.countHi2("xhi");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test8CountHi2() {
+        int result = TestClass.countHi2("hi");
+        org.junit.Assert.assertEquals(1, result);
+    }
+
+    @org.junit.Test
+    public void test9CountHi2() {
+        int result = TestClass.countHi2("xhixhi");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test10CountHi2() {
+        int result = TestClass.countHi2("hixhi");
+        org.junit.Assert.assertEquals(1, result);
+    }
+
+    // count8 tests
+    @org.junit.Test
+    public void test1Count8() {
+        int result = TestClass.count8(8818);
+        org.junit.Assert.assertEquals(4, result);
+    }
+
+    @org.junit.Test
+    public void test2Count8() {
+        int result = TestClass.count8(818);
+        org.junit.Assert.assertEquals(2, result);
+    }
+
+    @org.junit.Test
+    public void test3Count8() {
+        int result = TestClass.count8(8);
+        org.junit.Assert.assertEquals(1, result);
+    }
+
+    @org.junit.Test
+    public void test4Count8() {
+        int result = TestClass.count8(123);
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test5Count8() {
+        int result = TestClass.count8(81238);
+        org.junit.Assert.assertEquals(2, result);
+    }
+
+    @org.junit.Test
+    public void test6Count8() {
+        int result = TestClass.count8(88888);
+        org.junit.Assert.assertEquals(9, result);
+    }
+
+    @org.junit.Test
+    public void test7Count8() {
+        int result = TestClass.count8(0);
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test8Count8() {
+        int result = TestClass.count8(181818);
+        org.junit.Assert.assertEquals(3, result);
+    }
+
+    @org.junit.Test
+    public void test9Count8() {
+        int result = TestClass.count8(278);
+        org.junit.Assert.assertEquals(1, result);
+    }
+
+    @org.junit.Test
+    public void test10Count8() {
+        int result = TestClass.count8(12345);
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    // strCount tests
+    @org.junit.Test
+    public void test1StrCount() {
+        int result = TestClass.strCount("catcowcat", "cat");
+        org.junit.Assert.assertEquals(2, result);
+    }
+
+    @org.junit.Test
+    public void test2StrCount() {
+        int result = TestClass.strCount("catcowcat", "cow");
+        org.junit.Assert.assertEquals(1, result);
+    }
+
+    @org.junit.Test
+    public void test3StrCount() {
+        int result = TestClass.strCount("catcowcat", "dog");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test4StrCount() {
+        int result = TestClass.strCount("aaaa", "aa");
+        org.junit.Assert.assertEquals(2, result);
+    }
+
+    @org.junit.Test
+    public void test5StrCount() {
+        int result = TestClass.strCount("aaaa", "a");
+        org.junit.Assert.assertEquals(4, result);
+    }
+
+    @org.junit.Test
+    public void test6StrCount() {
+        int result = TestClass.strCount("", "a");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test7StrCount() {
+        int result = TestClass.strCount("abc", "");
+        org.junit.Assert.assertEquals(0, result);
+    }
+
+    @org.junit.Test
+    public void test8StrCount() {
+        int result = TestClass.strCount("ababab", "ab");
+        org.junit.Assert.assertEquals(3, result);
+    }
+
+    @org.junit.Test
+    public void test9StrCount() {
+        int result = TestClass.strCount("ababab", "aba");
+        org.junit.Assert.assertEquals(1, result);
+    }
+
+    @org.junit.Test
+    public void test10StrCount() {
+        int result = TestClass.strCount("aaaaa", "aa");
+        org.junit.Assert.assertEquals(2, result);
+    }
+
+    
 }
